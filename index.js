a0f1e7f07817912bf86a72b85a94a9323b2c414c
Alex de Freitas <alexdefreitas99@hotmail.com>
2020-01-29 10:45:20 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+var WebSocketClient = require('websocket').client;
+var client = new WebSocketClient();
+
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+
+client.on('connect', function(connection) {
+    console.log('WebSocket Client Connected');
+    connection.on('error', function(error) {
+        console.log("Connection Error: " + error.toString());
+    });
+
+    connection.on('close', function() {
+        console.log('echo-protocol Connection Closed');
+    });
+
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log("Received: '" + message.utf8Data + "'");
+        }
+    });
+
+    sendHostData();
+});
+
+function sendHostData() {
+    var hostData = {
+        hostName="alex"
+    }
+    connection.send(JSON.stringify(hostData));
+}
+
+client.connect('ws://localhost:8080/', 'echo-protocol');
